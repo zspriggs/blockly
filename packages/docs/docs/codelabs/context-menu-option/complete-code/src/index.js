f4e30335b6ef58b13503bf56704e62afd6535966
Zoë Spriggs <zoe.spriggs@raspberrypi.org>
2026-06-30 12:42:35 -0400

    docs: refactor context-menu codelab to use starter app

--- packages/docs/docs/codelabs/context-menu-option/complete-code/src/index.js
+++ packages/docs/docs/codelabs/context-menu-option/complete-code/src/index.js
@@ -1,20 +1,73 @@
-'use strict';
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-let workspace = null;
+import * as Blockly from 'blockly';
+import {blocks} from './blocks/text';
+import {forBlock} from './generators/javascript';
+import {javascriptGenerator} from 'blockly/javascript';
+import {save, load} from './serialization';
+import {toolbox} from './toolbox';
+import './index.css';
 
-function start() {
-  registerHelloWorldItem();
-  registerHelpItem();
-  registerDisplayItem();
-  Blockly.ContextMenuRegistry.registry.unregister('workspaceDelete');
-  registerSeparators();
+// Register the blocks and generator with Blockly
+Blockly.common.defineBlocks(blocks);
+Object.assign(javascriptGenerator.forBlock, forBlock);
 
-  Blockly.ContextMenuItems.registerCommentOptions();
-  // Create main workspace.
-  workspace = Blockly.inject('blocklyDiv', {
-    toolbox: toolboxSimple,
-  });
-}
+// Register context menu items
+registerHelloWorldItem();
+registerHelpItem();
+registerDisplayItem();
+Blockly.ContextMenuRegistry.registry.unregister('workspaceDelete');
+registerSeparators();
+Blockly.ContextMenuItems.registerCommentOptions();
+
+// Set up UI elements and inject Blockly
+const codeDiv = document.getElementById('generatedCode').firstChild;
+const outputDiv = document.getElementById('output');
+const blocklyDiv = document.getElementById('blocklyDiv');
+const ws = Blockly.inject(blocklyDiv, {toolbox});
+
+// This function resets the code and output divs, shows the
+// generated code from the workspace, and evals the code.
+// In a real application, you probably shouldn't use `eval`.
+const runCode = () => {
+  const code = javascriptGenerator.workspaceToCode(ws);
+  codeDiv.innerText = code;
+
+  outputDiv.innerHTML = '';
+
+  eval(code);
+};
+
+// Load the initial state from storage and run the code.
+load(ws);
+runCode();
+
+// Every time the workspace changes state, save the changes to storage.
+ws.addChangeListener((e) => {
+  // UI events are things like scrolling, zooming, etc.
+  // No need to save after one of these.
+  if (e.isUiEvent) return;
+  save(ws);
+});
+
+// Whenever the workspace changes meaningfully, run the code again.
+ws.addChangeListener((e) => {
+  // Don't run the code when the workspace finishes loading; we're
+  // already running it once when the application starts.
+  // Don't run the code during drags; we might have invalid state.
+  if (
+    e.isUiEvent ||
+    e.type == Blockly.Events.FINISHED_LOADING ||
+    ws.isDragging()
+  ) {
+    return;
+  }
+  runCode();
+});
 
 function registerHelloWorldItem() {
   const helloWorldItem = {
@@ -38,7 +91,6 @@ function registerHelloWorldItem() {
     id: 'hello_world',
     weight: 100,
   };
-  // Register.
   Blockly.ContextMenuRegistry.registry.register(helloWorldItem);
 }
 
