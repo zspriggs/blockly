2697072d1fc8b46602a5d6059da20a9efa163e9c
Zoë Spriggs <zoe.spriggs@raspberrypi.org>
2026-07-09 10:49:50 -0400

    chore: format

--- packages/docs/docs/codelabs/theme-extension-identifier/complete-code/src/index.js
+++ packages/docs/docs/codelabs/theme-extension-identifier/complete-code/src/index.js
@@ -5,11 +5,11 @@
  */
 
 import * as Blockly from 'blockly';
-import {blocks} from './blocks/text';
-import {forBlock} from './generators/javascript';
-import {javascriptGenerator} from 'blockly/javascript';
-import {save, load} from './serialization';
-import {toolbox} from './toolbox';
+import { blocks } from './blocks/text';
+import { forBlock } from './generators/javascript';
+import { javascriptGenerator } from 'blockly/javascript';
+import { save, load } from './serialization';
+import { toolbox } from './toolbox';
 import './index.css';
 
 // Register the blocks and generator with Blockly
